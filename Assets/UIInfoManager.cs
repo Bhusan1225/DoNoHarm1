88e72df46305b9cd7527b3999e1758d9ae983531
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-01-07 09:14:31 +0530

    .

--- Assets/UIInfoManager.cs
+++ Assets/UIInfoManager.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIInfoManager : MonoBehaviour
 {
+    [Header("Game Start panel")]
+    [Header("Buttons")]
+
+    public Button BackButton;
+   
+
+    [Header("Scene Names")]
+    public string PlayLevelName;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BackButton.onClick.AddListener(PlayGame);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void PlayGame()
+    {
+        SceneManager.LoadScene(PlayLevelName);
+    }
+
+   
 }
