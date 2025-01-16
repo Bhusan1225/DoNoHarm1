e23249711ba88da62f68ec82087274a22ec90c2c
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-01-16 09:36:16 +0530

    .

---
+++ Assets/functionCheck.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class functionCheck : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        Debug.Log("Awake called!");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called!");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update called!");
+    }
+}
+
